a38ed0b7791055f703ed0f9b1137b78927d7efdf
Manoel Amaro <mano.lira@gmail.com>
2013-05-12 10:08:13 -0300

    new layout

--- app/admin/syndics.rb
+++ app/admin/syndics.rb
@@ -8,6 +8,15 @@ ActiveAdmin.register Syndic do
         f.input :password
         f.input :password_confirmation
       end
+      f.inputs "Address", for: [:address, f.object.address || Address.new] do |address_form|
+        address_form.input :street
+        address_form.input :number
+        address_form.input :district
+        address_form.input :city
+        address_form.input :state
+        address_form.input :zip_code
+        address_form.input :complement
+      end
       f.has_many :phones do |add_f|
         add_f.inputs "Phone" do
           add_f.input :number
